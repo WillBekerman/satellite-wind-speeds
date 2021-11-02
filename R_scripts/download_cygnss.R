4a2a50a8b42e437ac45443e9a291f46914e98109
William Bekerman <wjb239@cornell.edu>
2021-11-02 18:11:57 -0400

    testing download_cygnss.R
    testing download_cygnss.R

--- R_scripts/download_cygnss.R
+++ R_scripts/download_cygnss.R
@@ -1,5 +1,5 @@
 
-setwd("../data/downloaded_cygnss")
+setwd("../data/fake_downloaded_cygnss")
 
 #args <- c("2020","1","2")
 args <- commandArgs(trailingOnly=TRUE)
@@ -18,18 +18,18 @@ varlist <- paste(vars,collapse=",")
 
 url_stem <- "https://podaac-opendap.jpl.nasa.gov/opendap/allData/cygnss/L2/v3.0/"
 
-for(j in cycle_start:cycle_end){
+for(j in 1:length(cycle_start:cycle_end)){
     
-    j3 <- sprintf("%03d",j)
+    j3 <- sprintf("%03d",cycle_start+j-1)
     cat(paste0("Cycle: ",j3,"\n"))
 
     ss <- paste0("s",format(date_start+j-1,"%Y%m%d"),"-000000-")
     ee <- paste0("e",format(date_start+j-1,"%Y%m%d"),"-235959")
-    fname <- paste0("cyg.ddmi.",ss,ee,".l2.wind-mss.a30.d31.nc.nc4")
+    fname <- paste0("cyg.ddmi.",ss,ee,".l2.wind-mss.a30.d31.nc.nc")
 
     com <- paste0(
         "wget --http-user=wjb239 --http-password=5NGvYoziBgij2TMp9f5 -r ",
-        "-nc -np -nd -A *.nc4 ",
+        "-nc -np -nd -A *.nc ",
         url_stem,year,"/",j3,"/",fname,"?",varlist
     )
     com <- paste0(
@@ -38,7 +38,7 @@ for(j in cycle_start:cycle_end){
     )
     print(com)
     system( com )
-    file.copy( paste0(fname,"?",varlist), fname )
+    file.copy( paste0(fname,"?",varlist), substr(fname,1,nchar(fname)-3) )
     file.remove( paste0(fname,"?",varlist) )
 
 }
