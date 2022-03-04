76a6ac126e0f556f83b1a4958dd94a3ada6f4967
WillBekerman <wjb239@cornell.edu>
2022-03-04 15:21:01 -0500

    removed unnecessary files

--- generate_start_end_dates.R
+++
@@ -1,5 +0,0 @@
-start_dates = seq( as.Date("2019-09-28"), to = as.Date("2020-09-28"), by = 'day' )[ seq(from = 1, by = 7, length.out = 52) ]
-end_dates = seq( as.Date("2019-09-28"), to = as.Date("2020-09-28"), by = 'day' )[ seq(from = 1, by = 7, length.out = 52) ] + 6
-
-dates_mat = noquote(cbind(as.character(start_dates), as.character(end_dates)))
-write.table(dates_mat, file="start_end_dates.txt", quote = F, row.names=FALSE, col.names=FALSE)
